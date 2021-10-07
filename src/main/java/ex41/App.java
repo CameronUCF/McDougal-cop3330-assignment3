9f28ff682cc5791273384e94d6c9a89e590dffdd
Geetis <capiix1@gmail.com>
2021-10-07 17:06:14 -0400

    ex41

--- src/main/java/ex41/App.java
+++ src/main/java/ex41/App.java
@@ -1,4 +1,52 @@
 package ex41;
 
-public class App {
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        ArrayList<String> namesList = readNames("./src/main/java/ex41/exercise41_input.txt");
+        Collections.sort(namesList);
+        printNames(namesList);
+    }
+
+    public static ArrayList<String> readNames(String fileName)
+    {
+        ArrayList<String> namesList = new ArrayList<String>();
+
+        try
+        {
+            File readFile = new File(fileName);
+            Scanner scanner = new Scanner(readFile);
+
+            while(scanner.hasNextLine())
+            {
+                namesList.add(scanner.nextLine());
+            }
+            scanner.close();
+
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return namesList;
+    }
+
+    public static void printNames(ArrayList<String> namesList)
+    {
+        System.out.printf("Total of %d names\n-----------------\n", namesList.size());
+
+        for(int i = 0; i < namesList.size(); i++)
+        {
+            System.out.printf("%s\n", namesList.get(i));
+        }
+    }
 }
