974d9d73c67123da1e36ac14bc021220a2ba0799
Geetis <capiix1@gmail.com>
2021-10-11 04:38:10 -0400

    ex44 - finished

--- src/main/java/ex44/App.java
+++ src/main/java/ex44/App.java
@@ -1,38 +1,58 @@
 package ex44;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonReader;
+
+import javax.tools.Diagnostic;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Scanner;
 
 public class App
 {
     public static void main( String[] args )
     {
-        Gson gson = new Gson();
-        try(Reader reader = new FileReader("./src/main/java/ex44/exercise44_input.json"))
+        int index = -1;
+        boolean found = false;
+        Scanner scanner = new Scanner(System.in);
+        ProductList products = LoadJSON("./src/main/java/ex44/exercise44_input.json");
+
+
+        while (index == -1)
         {
-            ProductList products = gson.fromJson(reader, ProductList.class);
-            System.out.println(products.products[0].name);
+            System.out.println("What is the product name?");
+            index = IsFound(products, scanner.nextLine());
+            if(index == -1)
+                System.out.println("Sorry, that product was not found in our inventory.");
+        }
+
+        System.out.printf("Name: %s\nPrice: %.2f\nQuantity: %d", products.products[index].name, products.products[index].price, products.products[index].quantity);
+    }
+
+    public static ProductList LoadJSON(String filePath)
+    {
+        Gson gson = new Gson();
+        ProductList products;
+
+        try(Reader reader = new FileReader(filePath))
+        {
+            products = gson.fromJson(reader, ProductList.class);
+            return products;
         }
         catch (IOException e)
         {
             e.printStackTrace();
         }
-        /*try
-        {
-            Product product = gson.fromJson(new FileReader("./src/main/java/ex44/exercise44_input.json"), Product.class);
-        }
-        catch (FileNotFoundException e)
-        {
-            e.printStackTrace();
-        }*/
+
+        return null;
+    }
+
+    public static int IsFound(ProductList proList, String query)
+    {
+        for(int i = 0; i < proList.products.length; i++)
+            if(proList.products[i].name.equalsIgnoreCase(query))
+                return i;
+        return -1;
     }
 }
 
